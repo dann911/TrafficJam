bbe0d6477a81454e2b5c8c3ab66dc6ecb285d74b
dann911 <shyoff21@yahoo.com>
2022-03-24 12:45:23 +0200

    Introduced car class

--- chestionar auto interactiv/Car.cpp
+++ chestionar auto interactiv/Car.cpp
@@ -1,1 +1,29 @@
 #include "Car.h"
+
+// Private functions
+
+
+
+// Constructors / Destructors
+
+Car::Car()
+{
+
+}
+
+Car::~Car()
+{
+
+}
+
+//Functions
+
+void Car::update()
+{
+
+}
+
+void Car::reender(sf::RenderWindow* window)
+{
+
+}
--- chestionar auto interactiv/Car.h
+++ chestionar auto interactiv/Car.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include "Game.h"
+
 class Car
 {
+private:
+
+
+
+public:
+	// Constructors / Destructors
+
+	Car();
+	virtual ~Car();
+
+	//Functions
+
+	void update();
+	void reender(sf::RenderWindow* window);
 };
 
