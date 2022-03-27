3014acad01a9775a854d66eb841dd7cbf0012f19
liviu274 <liviululi@gmail.com>
2022-03-27 23:22:43 +0300

    ADDED THE GAME ALGORITHM

--- chestionar auto interactiv/Game.cpp
+++ chestionar auto interactiv/Game.cpp
@@ -7,8 +7,11 @@ void Game::initVariables()
 {
 	this->window = nullptr;
 	std::srand(time(0));
-	this->NumberOfCars = std::rand() % 3 + 2;
+	this->NumberOfCars = std::rand() % 2 + 2;
 	std::cout << NumberOfCars << std::endl;
+
+	this->decision = 0 + (rand() % (1 - 0 + 1)) == 1;
+	std::cout << "Descision is: " << this->decision << std::endl;
 }
 
 void Game::initWindow()
@@ -26,8 +29,8 @@ void Game::initWindow()
 void Game::initTextures()
 {
     //Load map texture from file
-	if (this->texture.loadFromFile("Textures/basic intersection v2.PNG") == false)
-		std::cout << "map not found" << std::endl;
+	this->textures["MAP"] = new sf::Texture();
+	this->textures["MAP"]->loadFromFile("Textures/basic intersection v2.PNG");
 
 	this->textures["CARBLUE-HOR"] = new sf::Texture();
 	if (this->textures["CARBLUE-HOR"]->loadFromFile("Textures/car1-horizontal.png") == false)
@@ -43,13 +46,13 @@ void Game::initTextures()
 	this->textures["CARGREEN-VER"]->loadFromFile("Textures/carGREEN-vertical.png");
 
 	this->textures["PING"] = new sf::Texture();
-	this->textures["PING"]->loadFromFile("Textures/ping.png");
+	this->textures["PING"]->loadFromFile("Textures/Ping.png");
 }
 
 void Game::initSprites()
 {
 	//Set the map texture to the sprite
-	this->sprite.setTexture(this->texture);
+	this->sprite.setTexture(*textures["MAP"]);
 	this->sprite.setPosition(sf::Vector2f(120.f, 90.f));
 	this->sprite.setScale(0.7, 0.7);
 
@@ -58,22 +61,103 @@ void Game::initSprites()
 
 void Game::initCars()
 {
-	this->cars.push_back(new Car(this->textures["CARRED-VER"], car_positions[1 - 1].first, car_positions[1 - 1].second, 0.25, 2));
-	this->cars.push_back(new Car(this->textures["CARBLUE-HOR"], car_positions[2 - 1].first, car_positions[2 - 1].second, -0.25, 4));
-	this->cars.push_back(new Car(this->textures["CARGREEN-VER"], car_positions[3 - 1].first, car_positions[3 - 1].second, -0.25, 6));
-	this->cars.push_back(new Car(this->textures["CARYELLOW-HOR"], car_positions[4 - 1].first, car_positions[4 - 1].second, 0.25, 8));
+	this->cars.push_back(std::make_pair(new Car(this->textures["CARRED-VER"], car_positions[1 - 1].first, car_positions[1 - 1].second, 0.25), 1));
+	this->cars.push_back(std::make_pair(new Car(this->textures["CARBLUE-HOR"], car_positions[2 - 1].first, car_positions[2 - 1].second, -0.25), 3));
+	this->cars.push_back(std::make_pair(new Car(this->textures["CARGREEN-VER"], car_positions[3 - 1].first, car_positions[3 - 1].second, -0.25), 5));
+	this->cars.push_back(std::make_pair(new Car(this->textures["CARYELLOW-HOR"], car_positions[4 - 1].first, car_positions[4 - 1].second, 0.25), 7));
 
 	std::random_shuffle(std::begin(cars), std::end(cars));
 
+	this->direction = this->cars[cars.size() - 1].second - 1;
+	std::cout << "Direction: " << direction << std::endl;
+
 	while (this->cars.size() > this->NumberOfCars)
 		cars.pop_back();
+	this->NumberOfCars = this->cars.size();
 }
 
 void Game::initPing()
 {
 	this->ping.setTexture(*textures["PING"]);
-
+	this->ping.setPosition(ping_positions[direction / 2].first, ping_positions[direction / 2].second);
 }
+
+// Game algorithm private functions
+
+void Game::output()
+{
+	std::cout << "The PRIORITY list for the selected root is: ";
+
+	for (int i = 0; i < answer_list.size(); i++)
+	{
+		std::cout << this->answer_list[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+void Game::givePriority()
+{
+	int random_pos = rand() % NumberOfCars;
+	std::cout << "Number of cars: " << NumberOfCars << std::endl;
+	this->priority.push_back(cars[random_pos].second);
+	std::cout << "PRIORITY given to: " << cars[random_pos].second << std::endl;
+
+	this->cars[random_pos].second = INT_MAX;
+
+	for (int i = 0; i < this->cars.size(); i++)
+	{
+		if (abs(cars[i].second - priority[0]) == 4)
+		{
+			this->priority.push_back(cars[i].second);
+			std::cout << "PRIORITY given to: " << this->cars[i].second << std::endl;
+			cars[i].second = INT_MAX;
+
+			if (abs(priority[0] - direction) < abs(priority[1] - direction))   //trebuie sortat descrescator pentru ca prin push front se insereaza invers
+				std::swap(priority[0], priority[1]);
+			break;
+		}
+	}
+}
+
+void Game::getPriorityOrder()
+{
+	if (this->direction == 0)
+		this->direction = 8;
+
+	if (decision == 1)
+	{
+		this->givePriority();
+	}
+
+	for (int i = 0; i < this->cars.size(); i++)
+	{
+		if (this->cars[i].second != INT_MAX)
+		{
+			this->answer_list.push_back(cars[i].second);
+		}
+	}
+	int phase = 0;
+
+	sort(begin(answer_list), end(answer_list), [](int a, int b) {return a > b; });
+	if (this->answer_list.size() != 0)
+	{
+		while (this->answer_list[0] > this->direction && phase < this->answer_list.size())    //inverted closkwise listg sort
+		{
+			this->answer_list.push_back(this->answer_list[0]);
+			this->answer_list.pop_front();
+			phase++;
+		}
+	}
+	
+	this->output();
+
+	for (int i = 0; i < priority.size(); i++)
+	{
+		this->answer_list.push_front(this->priority[i]);
+	}
+	this->output();
+}
+
 
 
 
@@ -87,6 +171,7 @@ Game::Game()
 	this->initSprites();
 	this->initCars();
 	this->initPing();
+	this->getPriorityOrder();
 }
 
 Game::~Game()
@@ -98,8 +183,8 @@ Game::~Game()
 	for (auto& i : this->textures)
 		delete i.second;
 
-	for (auto* i : this->cars)
-		delete i;
+	for (auto& i : this->cars)
+		delete i.first;
 }
 
 const bool Game::running() const
@@ -108,6 +193,7 @@ const bool Game::running() const
 }
 
 //Functions
+
 
 void Game::pollEvents()
 {
@@ -131,7 +217,7 @@ void Game::update()
 {
 	this->pollEvents();
 	//Mouse Position relative to the window
-	std::cout << "Mouse Pos: " << sf::Mouse::getPosition(*this->window).x << " " <<sf::Mouse::getPosition(*this->window).y << "\n";
+	//std::cout << "Mouse Pos: " << sf::Mouse::getPosition(*this->window).x << " " <<sf::Mouse::getPosition(*this->window).y << "\n";
 }
 
 void Game::render()
@@ -151,10 +237,11 @@ void Game::render()
 	// Draw game map
 	this->window->draw(this->sprite);
 	// Draw game objects
-	for (auto* car : this->cars)
+	for (auto& car : this->cars)
 	{
-		car->reender(this->window);
+		car.first->render(this->window);
 	}
+	this->window->draw(this->ping);
 
 	this->window->display();
 }
